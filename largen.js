b8f73ac6150d203b5eb227ef4c133e6291b62b0d
ayush patidar <ayushghoda4444@gmail.com>
2023-11-10 20:28:03 +0530

    fifth file added

--- largen.js
+++ largen.js
@@ -1,11 +1,16 @@
+let num1 = 56;
+let num2 = 90;
+let num3 = 115;
+let largestNum;
 
+if (num1 > num2 && num1 > num3) {
+  largestNum = num1;
+} 
+else if (num2 > num1 && num2 > num3) {
+  largestNum = num2;
+} 
+else {
+  largestNum = num3;
+}
 
-const num1 = 9; 
-const num2 = 81; 
-
-console.log('SQR of ' +  
-    num1 + ' is ' + Math.sqrt(num1)); 
-    
-
-console.log('SQR of ' +  
-    num2 + ' is ' + Math.sqrt(num2)); 
+console.log(largestNum);
