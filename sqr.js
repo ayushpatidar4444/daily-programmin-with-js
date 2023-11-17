7ed2bbc53fb3a61696c8455c093f7349f2ea9dde
ayush patidar <ayushghoda4444@gmail.com>
2023-11-17 20:50:15 +0530

    file added

---
+++ sqr.js
@@ -0,0 +1,11 @@
+
+
+const num1 = 144; 
+const num2 = 169; 
+
+console.log('SQR of ' +  
+    num1 + ' is ' + Math.sqrt(num1)); 
+    
+
+console.log('SQR of ' +  
+    num2 + ' is ' + Math.sqrt(num2)); 
